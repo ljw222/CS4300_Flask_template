8471a72cf09a9e56cf56a33d7b05e4efa9b6cfa2
Lucy <ljw222@cornell.edu>
2021-05-02 13:38:34 -0400

    search preferences + refined dropdown

--- application.py
+++ application.py
@@ -12,15 +12,12 @@ app = Flask(__name__, template_folder='app/templates')
 def query():
   data = []
   output_message = ''
-  filter_message = ''
+  filter_message = {"Price Point": "", "Cuisine": "", "Ambiance": "", "Ambiance Weight": "", "Review Weight": ""}
 
   restaurant_query = request.args.get('fav_name')
   price_query = request.args.get('max_price')
   cuisine_query = request.args.get('cuisine')
   user_review = request.args.get('user_review')
-
-  print("query")
-  print(restaurant_query)
 
   # get ambiances
   ambiances_query = []
@@ -38,9 +35,29 @@ def query():
 
   if cuisine_query == None:
     cuisine_query = ""
-  # if ambiance_query == None:
-  #   ambiance_query = ""
 
+  # create filter feedback message
+  if len(ambiances_query) > 0:
+    ambiances_query_capitalized = map(str.capitalize, ambiances_query)
+    filter_message["Ambiance"] = ', '.join(ambiances_query_capitalized)
+  else:
+    filter_message["Ambiance"] = "None"
+
+  if cuisine_query == "":
+    filter_message["Cuisine"] = "None"
+  else:
+    filter_message["Cuisine"] = cuisine_query
+
+  if price_query == None:
+    filter_message["Price Point"] = "None"
+  else:
+    filter_message["Price Point"] = price_query.capitalize()
+
+  filter_message["Ambiance Weight"] = round(ambiance_weight, 2)
+  filter_message["Review Weight"] = round(review_weight, 2)
+
+
+  print(filter_message)
   # if there is an input
   if restaurant_query:
     restaurant_query = restaurant_query
@@ -51,13 +68,13 @@ def query():
       top_restaurants = [x[0] for x in top_tuple]
       top_sim_scores = [x[1] for x in top_tuple]
       app.logger.critical("got restaurants")
-      output_message = "Your search: " + restaurant_query
+      output_message = "Your Search: " + restaurant_query
       data = web_scraping(top_restaurants, top_sim_scores, restaurant_to_index[restaurant_query])
       # create filtering message
 
     # restaurant_query is not in the data
     else:
-      #output_message = "Your search " + restaurant_query + " is not in the dataset. Please enter its information"
+      output_message = "Your search: User Review"
       review_query = request.args.get('user_review')
       rel_restaurants = filterRestaurants(price_query, cuisine_query)
       cosine_sim_restaurants = computeCosine(review_query, rel_restaurants)
@@ -68,14 +85,10 @@ def query():
       app.logger.critical("got restaurants")
       output_message = "Your search: " + restaurant_query
       data = web_scraping(top_restaurants, top_sim_scores, 0)
-      #filter the restaurants that are relevant to the user's search
-      # rel_restaurants = filterRestaurants(price_query, cuisine_query)
-      # cosine_sim_restaurants = getCosineRestaurants(review_query, rel_restaurants)
-      #output_message = "Your search " + restaurant_query + " is not in the dataset. Please try another restaurant"
   legend_bool = True
   if len(data) == 0:
     legend_bool = False
-  return render_template('search.html', output_message=output_message, data=data, restaurant_list=restaurant_list, legend_bool = legend_bool)
+  return render_template('search.html', output_message=output_message, data=data, restaurant_list=restaurant_list, legend_bool=legend_bool, filter_message=filter_message)
 
 if __name__ == "__main__":
   print("Flask app running at http://0.0.0.0:5000")

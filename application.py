502a9c3948830f98ef69bbd6886b8fa190370ec4
Lucy <ljw222@cornell.edu>
2021-05-03 00:43:51 -0400

    fixed price preference output

--- application.py
+++ application.py
@@ -48,7 +48,7 @@ def query():
   else:
     filter_message["Cuisine"] = cuisine_query
 
-  if price_query == None:
+  if price_query == "":
     filter_message["Price Point"] = "None"
   else:
     filter_message["Price Point"] = price_query.capitalize()
